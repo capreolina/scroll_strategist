3e34bad4e1fec17e45dc0763b755e2839ba1bd87
deer <capreolina@protonmail.ch>
2021-04-19 10:47:16 +0000

    some cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
-use std::{cmp::Ordering, ops::Mul};
+use std::{cmp::Ordering, iter, ops::Mul};
 
+/// The state of an item, including how many slots it has left, and what its
+/// stats are. This is a node of a scrolling strategy tree, so it also can have
+/// (or may not have) a single "child" of type `ScrollUse`. This only supports
+/// one child at most, because we only want to keep the _optimal_ scroll usage
+/// in memory; others should be, and are, discarded.
+///
+/// This is an enum because we want a way to represent the "state" of an item
+/// that no longer exists, because it was boomed by a dark scroll.
 enum ItemState<'a> {
     Exists {
         slots: u8,
@@ -10,7 +18,9 @@ enum ItemState<'a> {
 }
 
 impl<'a> ItemState<'a> {
-    pub fn new_exists(slots: u8, stats: Stats) -> Self {
+    /// Creates a new instance of this type, specifically of the
+    /// `ItemState::Exists` variant. The child is defaulted to `None`.
+    pub const fn new_exists(slots: u8, stats: Stats) -> Self {
         Self::Exists {
             slots,
             stats,
@@ -18,19 +28,50 @@ impl<'a> ItemState<'a> {
         }
     }
 
-    pub fn new_boomed() -> Self {
+    /// Creates a new instance of this type, specifically of the
+    /// `ItemState::Boomed` variant. This contains no useful information other
+    /// than that the item was boomed.
+    pub const fn new_boomed() -> Self {
         Self::Boomed
     }
 }
 
+/// An instance of a particular scroll being used on a particular `ItemState`.
+/// Like `ItemState`, this type represents a part of a scrolling strategy tree;
+/// in particular, this represents a different kind of node than an `ItemState`
+/// does.
+///
+/// The child nodes here are themselves `ItemStates`, representing all possible
+/// outcomes of this scroll usage. The outcomes are stored in their own
+/// special-sauce type, `Outcomes`.
+///
+/// This struct contains a member representing the probability of reaching the
+/// goal given that this scroll is chosen (but not assuming any particular
+/// outcome of the scroll). There is also a member of this struct representing
+/// the expected cost (due solely to scroll expenditure) incurred due to this
+/// scroll being used, in addition to all future scrolls used. The future
+/// scroll costs are calculated optimally, as usual.
 struct ScrollUse<'a> {
+    /// "Probability of goal": Represents the probability of reaching the goal
+    /// given that this scroll is chosen (but not assuming any particular
+    /// outcome of the scroll).
     p_goal: f64,
+    /// "Expected cost": Represents the expected cost (due solely to scroll
+    /// expenditure) incurred due to this scroll being used, in addition to all
+    /// future scrolls used. The future scroll costs are calculated optimally,
+    /// as usual.
     exp_cost: f64,
+    /// The scroll being used.
     scroll: &'a Scroll,
+    /// All possible outcomes of this scroll usage; the children of this node.
     outcomes: Outcomes<'a>,
 }
 
 impl<'a> ScrollUse<'a> {
+    /// Creates a new scroll usage struct, given a particular scroll that is
+    /// being used. The probability of reaching the goal defaults to zero, the
+    /// expected cost defaults to the cost of `scroll`, and there are no
+    /// outcomes/children.
     pub const fn new(scroll: &'a Scroll) -> Self {
         Self {
             p_goal: 0.0,
@@ -41,18 +82,24 @@ impl<'a> ScrollUse<'a> {
     }
 }
 
+/// All possible outcomes of a particular scroll usage. Each outcome is
+/// represented as an `ItemState`. See the documentation for `ScrollUse` (and
+/// for `ItemState`) for more info.
 #[derive(Default)]
 struct Outcomes<'a> {
     outcomes: Vec<ItemState<'a>>,
 }
 
 impl<'a> Outcomes<'a> {
+    /// Creates a new empty set of outcomes.
     pub const fn new() -> Self {
         Self {
             outcomes: Vec::new(),
         }
     }
 
+    /// Adds a new outcome to this set of outcomes, and returns a mutable
+    /// reference to the newly added outcome.
     pub fn push_outcome(
         &mut self,
         outcome: ItemState<'a>,
@@ -63,6 +110,8 @@ impl<'a> Outcomes<'a> {
     }
 }
 
+/// A scroll. Contains all of the usual information associated with a scroll,
+/// in addition to its nominal cost.
 #[derive(Clone, Debug)]
 struct Scroll {
     /// Probability of success.
@@ -76,6 +125,9 @@ struct Scroll {
 }
 
 impl Scroll {
+    /// Creates a new scroll from the probability of success (`p_suc`), whether
+    /// or not the scroll is dark (`dark`), what the nominal cost of the scroll
+    /// is (`cost`), and what stats the scroll grants on success (`stats`).
     pub fn new(p_suc: f64, dark: bool, cost: f64, stats: Stats) -> Self {
         Self {
             p_suc,
@@ -85,6 +137,18 @@ impl Scroll {
         }
     }
 
+    /// Generates a "master scroll" based on a set of `Scroll`s. The master
+    /// scroll has a 100% probability of success, is not a dark scroll, has a
+    /// cost equal to [positive
+    /// infinity](https://en.wikipedia.org/wiki/Extended_real_number_line), and
+    /// grants a bonus to each stat that is equal to the highest bonus granted
+    /// to that stat by any of the scrolls in the input set.
+    ///
+    /// Basically, the `stats` member of the master scroll is generated by
+    /// summing up all of the `stats` members of the elements of the input set
+    /// (`scrolls`), if you think of each `Stats` struct as an element of a
+    /// [max tropical](https://en.wikipedia.org/wiki/Tropical_semiring)
+    /// [semimodule](https://en.wikipedia.org/wiki/Semimodule).
     pub fn master_scroll(scrolls: &[Self]) -> Self {
         let mut master =
             Self::new(1.0, false, f64::INFINITY, Default::default());
@@ -97,39 +161,59 @@ impl Scroll {
     }
 }
 
+/// An ordered array of stats that an item can have (q.v. `ItemState`), or that
+/// a scroll can grant on success (q.v. `Scroll`).
+///
+/// The elements of the stat array are not identified nominally, and can only
+/// be identified by their position/index within the array. As such, obviously,
+/// care must be taken when inserting elements into, and when copying to/from,
+/// the stats array. Particularly, the exact indices of the elements in a given
+/// stat array must be respected.
+///
+/// A `Stats`'s stat array is treated as implicitly infinite in length, with
+/// the "missing" elements implicitly being zeroes. As a result, any time that
+/// the length of such an array matters (like, for example, when summing two
+/// stat arrays together), some extra work is needed to treat any "missing"
+/// elements properly.
 #[derive(Clone, Debug, Default)]
 struct Stats {
     stats: Vec<u16>,
 }
 
 impl Stats {
+    /// Creates a new `Stats` using the provided stat array.
     pub const fn new_from_vec(stats: Vec<u16>) -> Self {
         Self { stats }
     }
 
+    /// Adds `self` to `other`, using ordinary addition, and returns the result
+    /// as a freshly-allocated `Stats`.
     pub fn plus(&self, other: &Self) -> Self {
-        let (longer, shorter) = if self.stats.len() >= other.stats.len() {
-            (&self.stats, &other.stats)
-        } else {
-            (&other.stats, &self.stats)
-        };
-
-        let mut stats = Vec::with_capacity(longer.len());
-
-        for (i, stat) in longer.iter().enumerate() {
-            stats.push(stat + shorter.get(i).unwrap_or(&0));
+        Self {
+            stats: self
+                .stats
+                .iter()
+                .chain(iter::repeat(&0))
+                .zip(other.stats.iter().chain(iter::repeat(&0)))
+                .map(|(s0, s1)| s0 + s1)
+                .take(self.stats.len().max(other.stats.len()))
+                .collect(),
         }
-
-        Self { stats }
     }
 
+    /// Performs summation of `self` with `other`, over the [max
+    /// tropical](https://en.wikipedia.org/wiki/Tropical_semiring)
+    /// [semimodule](https://en.wikipedia.org/wiki/Semimodule), but the result
+    /// is simply used to mutate `self` in-place.
+    ///
+    /// This function is used by `Scroll::master_scroll`.
     pub fn max_in_place(&mut self, other: &Stats) {
-        for (i, stat) in other.stats.iter().enumerate() {
-            if stat > self.stats.get(i).unwrap_or(&0) {
-                while i >= self.stats.len() {
-                    self.stats.push(0);
-                }
+        while other.stats.len() > self.stats.len() {
+            self.stats.push(0);
+        }
 
+        for (i, stat) in other.stats.iter().enumerate() {
+            if stat > &self.stats[i] {
                 self.stats[i] = *stat;
             }
         }
@@ -138,21 +222,16 @@ impl Stats {
 
 impl PartialEq for Stats {
     fn eq(&self, other: &Self) -> bool {
-        let (longer, shorter) = if self.stats.len() >= other.stats.len() {
-            (&self.stats, &other.stats)
-        } else {
-            (&other.stats, &self.stats)
-        };
-
-        for (i, stat) in longer.iter().enumerate() {
-            if stat != shorter.get(i).unwrap_or(&0) {
-                return false;
-            }
-        }
-
-        true
+        self.stats
+            .iter()
+            .chain(iter::repeat(&0))
+            .zip(other.stats.iter().chain(iter::repeat(&0)))
+            .take(self.stats.len().max(other.stats.len()))
+            .all(|(s0, s1)| s0 == s1)
     }
 }
+
+impl Eq for Stats {}
 
 impl PartialOrd for Stats {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
@@ -244,6 +323,14 @@ fn main() {
     }
 }
 
+/// Like other search functions in this program, this function assumes that
+/// `state` already has a well-defined value for `state.slots` and
+/// `state.stats`. Also, if `state.child.is_some()`, the value inside of
+/// `state.child` _will_ be ignored, and trampled/replaced. `scrolls` must be
+/// nonempty.
+///
+/// This function optimises _only_ to maximise the probability of reaching
+/// `goal`, going with lower expected costs only when needed to break a tie.
 fn solve_p<'a>(
     state: &mut ItemState<'a>,
     scrolls: &'a [Scroll],
